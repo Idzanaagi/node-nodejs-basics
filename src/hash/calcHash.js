66eb6ead4d47bf99a9681d9b924476925e8b466d
Idzanaagi <anmorgunowa@gmail.com>
2022-12-04 19:23:56 +0400

    feat: add solution to hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import path from 'path';
+import { createHash } from 'node:crypto';
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const pathToFile = path.join(__dirname, "files", "fileToCalculateHashFor.js");
+
 const calculateHash = async () => {
-    // Write your code here 
+    const messages = [pathToFile]
+    const hashe = messages.map(data => createHash('sha256').update(data).digest('hex'))
+    console.log(hashe[0])
 };
 
 await calculateHash();
