b7ddb2a5fdadeeb3d945216cbe7ee6077aa55a16
Idzanaagi <anmorgunowa@gmail.com>
2022-12-05 23:30:49 +0400

    add solution to stream tasks

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,28 @@
+import process from 'process';
+import * as fsp from "fs/promises";
+import path from 'path';
+const { stdin, stdout } = process;
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const pathToFile = path.join(__dirname, 'files', 'fileToWrite.txt');
+    stdout.write('Type your text\n')
+    
+    stdin.on('data', data => {    
+        const dataStringified = data.toString();
+        
+        fsp.appendFile(
+            pathToFile,
+            dataStringified,
+            (err) => {
+                if (err) throw err;
+            }
+        )
+    })
+        
 };
 
 await write();
