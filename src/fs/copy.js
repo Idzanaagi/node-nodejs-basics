a862d7c2b1f65ec81edc506b941706dd0f71fd85
Idzanaagi <anmorgunowa@gmail.com>
2022-12-06 00:07:32 +0400

    refactor: change fs tasks

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -5,22 +5,41 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const copy = async () => {
-  const pathtoCurrent = path.join(__dirname, "files");
-  const pathToDir = path.join(__dirname, "files_copy");
+const pathToDir = path.join(__dirname, "files_copy");
 
+const checkExist = async () => {
   try {
-    const files = await fsp.readdir(pathToDir);
-    console.error("FS operation failed");
-  } catch {
-    const files = await fsp.readdir(pathtoCurrent);
-    const dir = await fsp.mkdir(pathToDir, { recursive: true });
-    
-    for (const file of files) {
-      fsp.copyFile(path.join(pathtoCurrent, file), path.join(pathToDir, file));
-      console.log(`${file} is copy`);
+    await fsp.readdir(pathToDir);
+    if (true) {
+      return true;
     }
+  } catch {
+    return false;
   }
 };
 
-copy();
+const copy = async () => {
+  const pathtoCurrent = path.join(__dirname, "files");
+  const check = await checkExist();
+
+  try {
+    if (check) {
+      throw new Error("FS operation failed");
+    } else {
+      const files = await fsp.readdir(pathtoCurrent);
+      const dir = await fsp.mkdir(pathToDir, { recursive: true });
+
+      for (const file of files) {
+        fsp.copyFile(
+          path.join(pathtoCurrent, file),
+          path.join(pathToDir, file)
+        );
+        console.log(`${file} is copy`);
+      }
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+await copy();
